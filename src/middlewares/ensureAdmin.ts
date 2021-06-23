2e71bcd6e422aa95a4bf4785a2c4da13ec1d1693
georginmatheus <georginmatheus@gmail.com>
2021-06-23 10:37:27 -0300

    add verification middleware if user is admin

---
+++ src/middlewares/ensureAdmin.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const ensureAdmin = (
+  request: Request,
+  response: Response,
+  next: NextFunction): any => {
+  const admin = true
+
+  if (admin) {
+    return next()
+  }
+
+  return response.status(401).json({
+    error: 'Unauthorized'
+  })
+}
